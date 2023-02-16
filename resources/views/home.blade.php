cb5567107ecfad0b739564ea4613152c9409f840
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-02-16 12:10:24 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,6 +49,24 @@
                                  el: ".swiper-pagination",
                                  clickable: true,
                                  },
+                                 breakpoints: {
+          340: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+      },
                                  });
                               </script>
                               {{-- section danh muc --}}
